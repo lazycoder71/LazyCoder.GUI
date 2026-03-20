67d5e656f91bde67a9ab571a1094e64de74016ec
lazycoder71 <ndlam1994@gmail.com>
2026-03-20 16:28:12 +0700

    * GuiNavButton

--- Runtime/Nav/GuiNavButton.cs
+++ Runtime/Nav/GuiNavButton.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -6,13 +7,48 @@ namespace LazyCoder.Gui
 {
     public class GuiNavButton : GuiButton
     {
+        [Title("Page")]
+        [Required]
         [SerializeField] private AssetReference _page;
-        
+
+        [Title("Container")]
+        [SerializeField] private GuiNavPageContainer _container;
+
+        [ShowIf("@!_container")]
+        [SerializeField] private int _containerGroupIndex;
+
+        private bool _isOpening = false;
+
+        protected virtual GuiNavContext Context => null;
+
         public override void Button_OnClick()
         {
             base.Button_OnClick();
-            
-            GuiNav.PushPageAsync(_page).Forget();
+
+            PushPageAsync().Forget();
+        }
+
+        private async UniTask PushPageAsync()
+        {
+            if (_isOpening)
+                return;
+
+            _isOpening = true;
+
+            GuiNavPage page;
+
+            if (_container != null)
+                page = await GuiNav.PushPageAsync(_page, _container, Context);
+            else
+                page = await GuiNav.PushPageAsync(_page, _containerGroupIndex, Context);
+
+            _isOpening = false;
+
+            OnPagePushed(page);
+        }
+
+        protected virtual void OnPagePushed(GuiNavPage page)
+        {
         }
     }
 }
